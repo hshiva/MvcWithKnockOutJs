2912cb8f5d921282981b782614396ede365c59b7
hshiva <shivakumara.ckm@yahoo.com>
2016-08-23 00:05:30 +0530

    added model class

---
+++ MvcWithKnockOutJs/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcWithKnockOutJs.Models
+{
+    public class Contact
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public string no { get; set; }
+       
+    }
+}
---
+++ MvcWithKnockOutJs/ViewModel/ContactViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcWithKnockOutJs.ViewModel
+{
+    public class ContactViewModel
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public string no { get; set; }
+    }
+}
